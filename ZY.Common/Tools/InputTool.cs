23cd7f7477453004fff329cd1a8622babad28516
zhouyang <yzhou3_05@sina.com>
2017-02-20 11:31:03 +0800

    添加InputTool

---
+++ ZY.Common/Tools/InputTool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ADCC.Common.Tools
+{
+    public static class InputTool
+    { 
+        public static void ValidateFile(this Inputer inputer, string xsdFile)
+        {
+            if (!File.Exists(xsdFile))
+                throw new ArgumentNullException("The input file is not exist!");
+
+            string extendString = new FileInfo(xsdFile).Extension;
+            if (!string.Equals(extendString, inputer.Extend))
+                throw new ArgumentException("Please input .xsd file!");
+        }
+
+        public static string GetFileStream(this Inputer inputer, string xsdFile)
+        {
+            inputer.ValidateFile(xsdFile);
+            return "sss";
+        }
+    }
+
+    public class Inputer
+    {
+        public string Extend { get; private set; }
+        public Inputer(string extend)
+        {
+            Extend = extend;
+        }
+    }
+}
